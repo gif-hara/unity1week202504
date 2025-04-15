ae42bf5b37d17350c979f43190e931771fa0749e
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-04-15 23:43:54 +0900

    Add Define class with DanceType enum for dance direction management

---
+++ Assets/unity1week202504/Scripts/Define.cs
@@ -0,0 +1,13 @@
+namespace unity1week202504
+{
+    public static class Define
+    {
+        public enum DanceType
+        {
+            Left = 0,
+            Right = 1,
+            Up = 2,
+            Down = 3,
+        }
+    }
+}
