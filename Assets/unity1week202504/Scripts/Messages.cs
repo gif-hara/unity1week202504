405980ecf5b17a2caf5c044a836612bfffced9ae
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-04-16 00:40:31 +0900

    Refactor Messages class to remove unused using directive and add ListenDance struct for dance event handling

--- Assets/unity1week202504/Scripts/Messages.cs
+++ Assets/unity1week202504/Scripts/Messages.cs
@@ -1,11 +1,19 @@
-using UnityEngine;
-
 namespace unity1week202504
 {
     public static class Messages
     {
         public readonly struct Beat
         {
+        }
+
+        public readonly struct ListenDance
+        {
+            public Define.DanceType DanceType { get; }
+
+            public ListenDance(Define.DanceType danceType)
+            {
+                DanceType = danceType;
+            }
         }
     }
 }
