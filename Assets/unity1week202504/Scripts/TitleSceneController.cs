11154d7d124e6525c962153d3aa8a66eb3657dc1
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-04-18 00:42:10 +0900

    Add Title Scene and Audio Management to TitleSceneController
    - Created a new Title scene asset.
    - Updated TitleSceneController to manage background music and sound effects.
    - Integrated UI interactions for volume adjustments and scene transitions.
    - Added functionality to play background music and handle user input for starting the game.

--- Assets/unity1week202504/Scripts/TitleSceneController.cs
+++ Assets/unity1week202504/Scripts/TitleSceneController.cs
@@ -1,7 +1,9 @@
 using Cysharp.Threading.Tasks;
 using HK;
+using R3;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 namespace unity1week202504
 {
@@ -13,8 +15,29 @@ namespace unity1week202504
         [SerializeField]
         private AudioMixer audioMixer;
 
+        [SerializeField]
+        private AudioManager audioManagerPrefab;
+
         async UniTaskVoid Start()
         {
+            var uiViewTitle = new UIViewTitle(titleDocument);
+            var audioManager = Instantiate(audioManagerPrefab);
+            uiViewTitle.OnValueChangedBgmVolumeAsObservable()
+                .Subscribe(audioMixer, static (x, audioMixer) => audioMixer.SetFloat("BgmVolume", Mathf.Log10(x) * 20))
+                .RegisterTo(destroyCancellationToken);
+            uiViewTitle.OnValueChangedSfxVolumeAsObservable()
+                .Subscribe(audioMixer, static (x, audioMixer) => audioMixer.SetFloat("SfxVolume", Mathf.Log10(x) * 20))
+                .RegisterTo(destroyCancellationToken);
+
+            audioManager.PlayBgm("Bgm.Title", 0.0f);
+            uiViewTitle.CloseConfirm();
+            await uiViewTitle.PlayFadeAnimationAsync("In.1", destroyCancellationToken);
+            await uiViewTitle.OnClickPleaseAnyClickAsync(destroyCancellationToken);
+            uiViewTitle.ClosePleaseAnyClick(destroyCancellationToken);
+            uiViewTitle.OpenConfirm();
+            await uiViewTitle.OnClickGameStartButtonAsync(destroyCancellationToken);
+            await uiViewTitle.PlayFadeAnimationAsync("Out.1", destroyCancellationToken);
+            SceneManager.LoadScene("Game");
         }
     }
 }
